fa3448cc02e7dec5cc235b717a492e41f687394d
Chris Toomey <cmtoomey@users.noreply.github.com>
2017-11-22 12:07:58 -0800

    Create restart.py

---
+++ Scripts/restart.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+import json
+import urllib
+import boto3
+
+print('Loading function')
+
+ssm = boto3.client('ssm')
+
+
+def lambda_handler(event, context):
+    print('Running Cleanup')
+    response = ssm.send_command(
+        InstanceIds=['mi-08ec007336c8b7e81'],
+        DocumentName='AWS-RunPowerShellScript',
+        Parameters={ "commands":[ "tabadmin cleanup; tabadmin cleanup --restart" ]}
+    )
